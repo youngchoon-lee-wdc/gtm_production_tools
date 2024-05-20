aa94915e62e529077236385a9656c6c70165c643
youngchoon-lee-wdc <youngchoon.lee@wdc.com>
2024-05-20 18:53:07 +0900

    enhance to restart machine becuse low gen speed

--- get_gen_speed.ps1
+++ get_gen_speed.ps1
@@ -40,3 +40,56 @@ $genSpeedStr | Out-File -FilePath $filename
 
 # Output the result
 Write-Host "PCI current link speed saved to $filename"
+
+# Define the path to the file that stores the restart count
+$restartCountFile = "$targetDirectory\restart_count.txt"
+
+# Function to get the current restart count from the file
+function Get-RestartCount {
+    if (Test-Path $restartCountFile) {
+        return [int](Get-Content $restartCountFile)
+    } else {
+        return 0
+    }
+}
+
+# Function to increment and save the restart count
+function Increment-RestartCount {
+    $count = Get-RestartCount
+    $count++
+    Set-Content -Path $restartCountFile -Value $count
+    return $count
+}
+
+$minimumRequiredGenSpeed = 4
+$maxRestartCount = 3
+
+# Function to send an email notification
+function Send-EmailNotification {
+    $smtpServer = "milrelay.sandisk.com"
+    $smtpFrom = "your-email@yourdomain.com"
+    $smtpTo = "youngchoon.lee@wdc.com,SangMin.Seok2@wdc.com,Jeonghwa.Kim@wdc.com,minlee.ha@wdc.com"
+    $messageSubject = "GTM Machine Restart Alert"
+    $messageBody = "The machine has restarted more than $maxRestartCount times due to Gen spped is less than minimum required Gen$minimumRequiredGenSpeed"
+
+    Send-MailMessage -SmtpServer $smtpServer -From $smtpFrom -To $smtpTo -Subject $messageSubject -Body $messageBody
+}
+
+
+if ($pciLinkSpeed -lt $minimumRequiredGenSpeed) {
+    Write-Host "Gen speed is less than $minimumRequiredGenSpeed. Restarting the machine..."
+    $restartCount = Increment-RestartCount
+
+    if ($restartCount -gt $maxRestartCount) {
+        Write-Host "Restart count has exceeded $maxRestartCount. Sending email notification..."
+        Send-EmailNotification
+        Write-Host "Deleting the restart count file..."
+        Remove-Item -Path $restartCountFile -Force
+    }
+
+    Restart-Computer -Force
+} else {
+    Write-Host "Value is $minimumRequiredGenSpeed or greater. No action needed."
+}
+
+
