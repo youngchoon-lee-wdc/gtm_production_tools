dcde19702f325578a4a873b764c5f7d63f330d5c
youngchoon-lee-wdc <youngchoon.lee@wdc.com>
2024-06-05 17:42:58 +0900

    add restart machine

--- turnon_test_mode.bat
+++ turnon_test_mode.bat
@@ -8,5 +8,16 @@ echo Turned ON Test mode for next CVF production
 C:\Windows\System32\pnputil.exe /add-driver "C:\Program Files (x86)\SanDisk\CVF_2.0_x64\Driver\SDdriver_64bitWinXX\package\SdNvmeDriver.inf" /install
 echo Switched driver successfully (Standard NVM Standard NVM Express Controller --> SanDisk NVMe) 
 
-echo It's about to restart machine for next CVF production..
-echo %ERRORLEVEL%
+setlocal
+@REM Windows error code 3010 indicates that a reboot is necessary to complete the installation process
+if %errorlevel%==3010 (
+    echo Warning: The operation completed successfully, but a restart is required.
+    
+    REM Reset the errorlevel to 0
+    shutdown /r /t 5
+    exit /b 0
+    
+)
+echo %errorlevel%
+shutdown /r /t 5
+exit /b %errorlevel%
